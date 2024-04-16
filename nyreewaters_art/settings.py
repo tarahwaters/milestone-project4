1e9fe0111d6457aa90fe1b4d9840441c9a49af22
tarahwaters <thbwaters@outlook.com>
2024-04-16 16:52:09 +0000

    Set up new django secret key in secure env.py

--- nyreewaters_art/settings.py
+++ nyreewaters_art/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6(w%3y($(lmnnmtn8!vb6hk=%(s%h=(c!6qz*%ct!d&tt*6#da'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
