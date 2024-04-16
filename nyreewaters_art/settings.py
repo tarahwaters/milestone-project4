e134fd2de741af489ef119a6b14bcb8f2e6f017c
tarahwaters <thbwaters@outlook.com>
2024-04-16 16:57:47 +0000

    Fix configuration for django secret key from env.py

--- nyreewaters_art/settings.py
+++ nyreewaters_art/settings.py
@@ -1,4 +1,6 @@
 import os
+if os.path.exists("env.py"):
+    import env
 
 """
 Django settings for nyreewaters_art project.
@@ -13,8 +15,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
