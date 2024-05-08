b63e92bb635dab46ff1938756a26c85452e23835
tarahwaters <thbwaters@outlook.com>
2024-05-08 17:53:56 +0000

    Add an if statement to check whether a search query exists with django error message if blank

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Product
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
 
     context = {
         'products': products,
