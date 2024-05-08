ea95dc63d2e073c8708dfff32115216266cb0ec6
tarahwaters <thbwaters@outlook.com>
2024-05-08 16:38:21 +0000

    Fix typo on line 19 to define 'product'

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show individual product details """
     
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
