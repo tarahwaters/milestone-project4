34d5375556a007625bc77133a169136392ebf794
tarahwaters <thbwaters@outlook.com>
2024-05-09 16:20:33 +0000

    Set debug to True only if DEVELOPMENT env variable

--- nyreewaters_art/settings.py
+++ nyreewaters_art/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nyreewaters-art-ccb67c4ebd7f.herokuapp.com', 'localhost']
 
