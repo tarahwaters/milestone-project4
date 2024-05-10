2c899f55f7f257393f59b48f6296ad8a78a07b84
tarahwaters <thbwaters@outlook.com>
2024-05-10 09:48:11 +0000

    Add gitpod server to allowed hosts

--- nyreewaters_art/settings.py
+++ nyreewaters_art/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['nyreewaters-art-ccb67c4ebd7f.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', '8000-tarahwaters-milestonepr-yu8ehnrtdc5.ws-eu111.gitpod.io', 'nyreewaters-art-ccb67c4ebd7f.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
