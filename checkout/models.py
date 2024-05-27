6fe6240b4d287535b0ba5693a00da2a2a5b1f759
tarahwaters <thbwaters@outlook.com>
2024-05-27 12:44:40 +0000

    Add OrderLineItem class to models

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Order(models.Model):
-    
     order_number = models.CharField(max_length=32, null=False, editable=False)
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
@@ -20,3 +19,16 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    # automatically calculates each time the line item is saved
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
