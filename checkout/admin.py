83a239f909a5a9638a8471c1569ac25d9e7ac2cd
tarahwaters <thbwaters@outlook.com>
2024-05-27 14:15:13 +0000

    Import checkout models and add order classes to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'first_name', 'last_name',
+              'email', 'mobile_number','address_line1',
+              'address_line2', 'town_or_city', 'county', 
+              'country', 'postcode', 'date',
+              'delivery_cost', 'order_total', 
+              'grand_total',)
+
+    list_display = ('order_number', 'date', 'first_name',
+                    'last_name', 'email', 'mobile_number',
+                    'order_total', 'delivery_cost', 
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
