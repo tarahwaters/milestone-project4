cf2bde6e8f3433463ab0e1afc8428368ff599ebd
tarahwaters <thbwaters@outlook.com>
2024-05-27 12:40:43 +0000

    CCreate the model for class 'Order'

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    mobile_number = models.CharField(max_length=20, null=False, blank=False)
+    address_line1 = models.CharField(max_length=80, null=False, blank=False)
+    address_line2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
