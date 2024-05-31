737d3353f83618a068e03f46addf4afd732b801b
tarahwaters <thbwaters@outlook.com>
2024-05-31 10:47:34 +0000

    Add OrderForm import missing from checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
+from .forms import OrderForm
+
 
 def checkout(request):
     bag = request.session.get('bag', {})
