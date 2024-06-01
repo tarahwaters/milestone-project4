553951e5893717fb978651ae50206c144c84d9e7
tarahwaters <thbwaters@outlook.com>
2024-06-01 11:44:31 +0000

    Add checkout context for stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PMpxVL0vlTPTvrWTEKrLHExqbB3wtKLScc5jwGdsxAENiRxF6X6C4YyL9iAOTgXhywxmmxpTLYjf3qFktqjfVss00EC1XYxHH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
