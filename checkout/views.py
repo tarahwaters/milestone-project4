a5555c511ddf7801fb26d9c9c92a1bc3703b07a8
tarahwaters <thbwaters@outlook.com>
2024-06-03 15:29:01 +0000

    Remove print(intent) statement from checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -69,8 +69,6 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-        print(intent)
         
         order_form = OrderForm()
 
