97c73b6b85f6c45ac504480618bd8b1ad0a0fc4f
tarahwaters <thbwaters@outlook.com>
2024-06-03 13:46:52 +0000

    Install and import stripe settings, and set up stripe key variables for use

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
+import stripe
+
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment!")
@@ -14,6 +20,7 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
--- nyreewaters_art/settings.py
+++ nyreewaters_art/settings.py
@@ -183,8 +183,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 40
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.environ('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ('STRIPE_SECRET_KEY', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
