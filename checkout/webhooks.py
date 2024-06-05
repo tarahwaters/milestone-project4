4d7a2ff8f190cf0143d4a96155afbcc046b2c559
tarahwaters <thbwaters@outlook.com>
2024-06-05 13:55:18 +0000

    Add require_POST and csrf_exempt decorators

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,6 +7,8 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import Stripe
 
+@require_POST
+@csrf_exempt
 def webhook(request):
     """ Listen for webhooks from Stripe """
     payload = request.body
