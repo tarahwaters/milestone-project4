a94962580a44fb3e272574a003263909aaf524cc
tarahwaters <thbwaters@outlook.com>
2024-06-05 13:57:24 +0000

    Add wh_secret, stripe.api_key and descriptive comments

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,6 +11,11 @@ import Stripe
 @csrf_exempt
 def webhook(request):
     """ Listen for webhooks from Stripe """
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
