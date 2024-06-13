f7a3434cc6821a88fc0bf1911ac4753c6b88f3f8
Tarah Waters <thbwaters@outlook.com>
2024-06-13 15:37:10 +0000

    Setup webhook handler and event_map dictionary

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -33,5 +33,11 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
     
-    print('Success!')
-    return HttpResponse(status=200)
+# Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+# Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
