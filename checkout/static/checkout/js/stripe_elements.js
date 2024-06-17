25ae83b67b5268c3d2ee5ea5931a6aac634776be
Tarah Waters <thbwaters@outlook.com>
2024-06-17 11:06:48 +0000

    Add a failure function triggered by 400 back request response

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -114,5 +114,9 @@ form.addEventListener('submit', function(ev) {
                 }
             }
         });
+    }).fail(function () {
+        // triggered if view sends a 400 back request response -
+        // reloads the page, the error will be in django messages
+        location.reload();
     })
 });
