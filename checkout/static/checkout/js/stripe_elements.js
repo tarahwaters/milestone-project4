adc5483afbd7d3cbeb9db82fdae4978df58350c2
Tarah Waters <thbwaters@outlook.com>
2024-06-17 11:01:54 +0000

    Add checkout form data caching in payment intent (JS)

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,12 +53,36 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(client_secret, {
-        payment_method: {
-            card: card,
-            billing_details: {
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = 4('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': client_secret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: `${$.trim(form.first_name.value)} ${$.trim(form.last_name.value)}`,
+                    email: $.trim(form.email.value),
+                    phone: $.trim(form.mobile_number.value),
+                    address: {
+                        line1: $.trim(form.address_line1.value),
+                        line2: $.trim(form.address_line2.value),
+                        city: $.trim(form.town_or_city.value),
+                        state: $.trim(form.county.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+            shipping: {
                 name: `${$.trim(form.first_name.value)} ${$.trim(form.last_name.value)}`,
-                email: $.trim(form.email.value),
                 phone: $.trim(form.mobile_number.value),
                 address: {
                     line1: $.trim(form.address_line1.value),
@@ -66,40 +90,29 @@ form.addEventListener('submit', function(ev) {
                     city: $.trim(form.town_or_city.value),
                     state: $.trim(form.county.value),
                     country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                // Shows error to the customer (e.g. insufficient funds)
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                // Successful payment process
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
                 }
             }
-        },
-        shipping: {
-            name: `${$.trim(form.first_name.value)} ${$.trim(form.last_name.value)}`,
-            phone: $.trim(form.mobile_number.value),
-            address: {
-                line1: $.trim(form.address_line1.value),
-                line2: $.trim(form.address_line2.value),
-                city: $.trim(form.town_or_city.value),
-                state: $.trim(form.county.value),
-                country: $.trim(form.country.value),
-                postal_code: $.trim(form.postcode.value),
-            }
-        },
-    }).then(function(result) {
-        if (result.error) {
-            // Shows error to the customer (e.g. insufficient funds)
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            // Successful payment process
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
+        });
+    })
 });
