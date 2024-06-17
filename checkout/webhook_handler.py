af6e7e730850588b19f389098900b8828a85b284
Tarah Waters <thbwaters@outlook.com>
2024-06-17 11:25:25 +0000

    Replace empty strings in shipping details with None to keep db and form data the same

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -33,6 +33,11 @@ class StripeWH_Handler:
         shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2) # updated
 
+         # Clean data in the shipping details
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
         return HttpResponse(
             content=f'Webhook received: {event['type']}',
             status=200)
