3d84345a96840aad2cd75104cf7332297539bb98
Tarah Waters <thbwaters@outlook.com>
2024-06-25 13:31:26 +0000

    Add missing default first and last name fields to UserProfile model and migrate changes

--- profiles/models.py
+++ profiles/models.py
@@ -12,6 +12,8 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_first_name = models.CharField(max_length=40, null=True, blank=True)
+    default_last_name = models.CharField(max_length=40, null=True, blank=True)
     default_mobile_number = models.CharField(max_length=20, null=True, blank=True)
     default_address_line1 = models.CharField(max_length=80, null=True, blank=True)
     default_address_line2 = models.CharField(max_length=80, null=True, blank=True)
