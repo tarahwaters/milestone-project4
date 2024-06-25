cefc7ad56eae23ee270c99eddfb41dbb4111251d
Tarah Waters <thbwaters@outlook.com>
2024-06-25 13:19:23 +0000

    Add a comment to the save_info part of the checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -73,6 +73,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
+            # Save the info to the user's profile if all checks pass
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success',
                                     args=[order.order_number]))
