929f460f227911f67e2bb3b195cb508ed02087e6
Tarah Waters <thbwaters@outlook.com>
2024-06-27 12:33:30 +0000

    Adjust line breaks of checkout view imports for clarity

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
 from profiles.forms import UserProfileForm
 from profiles.models import UserProfile
