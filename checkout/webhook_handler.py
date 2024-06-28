71f0a9644d814f8e0f03a1681fbfa802af3b55b1
Tarah Waters <thbwaters@outlook.com>
2024-06-28 13:17:24 +0000

    Call function for sending confirmation email before returning responses to Stripe and fix typo

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,7 +18,7 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
     
-    def send_confirmation_email(self, order):
+    def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         customer_email = order.email
         subject = render_to_string(
@@ -108,6 +108,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                     content=f'Webhook received: {event['type']} | SUCCESS: Verified order already in database',
                     status=200)
@@ -144,7 +145,8 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)       
-            
+
+        self._send_confirmation_email(order)    
         return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
                 status=200)
