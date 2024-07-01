98df10c7d19ab1708869eeee49b8dbc1ca439ed7
Tarah Waters <thbwaters@outlook.com>
2024-07-01 16:13:47 +0000

    Update Product Admin form and create add_product template

--- products/forms.py
+++ products/forms.py
@@ -7,11 +7,11 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get.friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
