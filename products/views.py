6ae9bb3623450e74e615235e9d02af82a58a8548
Tarah Waters <thbwaters@outlook.com>
2024-07-04 11:33:38 +0000

    Add POST functionality and save for edit_product view

--- products/views.py
+++ products/views.py
@@ -96,8 +96,17 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
-    form = ProductForm(instance=product)
-    messages.info(request, f'You are editing item: "{product.name}"')
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect (reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing item: "{product.name}"')
 
     template = 'products/edit_product.html'
     context = {
