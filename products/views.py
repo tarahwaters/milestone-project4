4d48354377f1c6b09237b5052a1b65fc84ad8fd1
Tarah Waters <thbwaters@outlook.com>
2024-07-04 11:28:52 +0000

    Add edit_product template, view and url without POST functionality yet

--- products/views.py
+++ products/views.py
@@ -91,3 +91,18 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing item: "{product.name}"')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
