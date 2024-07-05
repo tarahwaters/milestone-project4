14326e0eaaa9f23ea6ff77b250151eba42a2d698
Tarah Waters <thbwaters@outlook.com>
2024-07-05 12:42:49 +0000

    Fix missing underscores in login_required decorators

--- products/views.py
+++ products/views.py
@@ -73,7 +73,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-@login required
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
@@ -99,7 +99,7 @@ def add_product(request):
     return render(request, template, context)
 
 
-@login required
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
@@ -128,13 +128,13 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
-@login required
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that')
         return redirect(reverse('home'))
-        
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
