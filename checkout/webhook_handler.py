b0c1c6bd4508980da88ee6548fc636574f4b6f5a
Tarah Waters <thbwaters@outlook.com>
2024-07-09 14:10:30 +0000

    Fix f'string error - missing double quotations around type line 112

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -109,7 +109,7 @@ class StripeWH_Handler:
                 time.sleep(1)
         if order_exists:
             self._send_confirmation_email(order)
-            return HttpResponse(content=f'Webhook received: {event['type']} | SUCCESS: Verified order already in database', status=200)
+            return HttpResponse(content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database', status=200)
         else:
             order = None
             try:
