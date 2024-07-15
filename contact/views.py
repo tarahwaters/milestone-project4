dd401d5f612bf13d607dd37ffa87ee90cccf3aa1
Tarah Waters <thbwaters@outlook.com>
2024-07-15 20:05:52 +0000

    Migrate model changes for Contact page and add view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    """ Return the contact form page """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message was sent successfully - thank you!')
+            return redirect('contact')
+        else:
+            messages.error(request, 'There was an error submitting the form. \
+                Please make sure the information is valid.')
+    form = ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact/contact.html', context)
