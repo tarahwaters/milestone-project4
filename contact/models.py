c2a90d0b187ab72086b713190b75e5014b621a4e
Tarah Waters <thbwaters@outlook.com>
2024-07-15 19:56:47 +0000

    Create model for Contact page

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=500)
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.email
