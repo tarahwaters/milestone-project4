e02bb97cf4344914ce00e22dbe05224e9316d901
Tarah Waters <thbwaters@outlook.com>
2024-07-17 13:41:26 +0000

    Add bespoke_enquiry view to handle the form

--- bespoke/views.py
+++ bespoke/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BespokeForm
 
-# Create your views here.
+def bespoke_enquiry(request):
+    if request.method == 'POST':
+        form = BespokeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bespoke_success')
+    else:
+        form = BespokeForm()
+    return render(request, 'bespoke/bespoke_enquiry.html', {'form': form})
+
+def bespoke_success(request):
+    return render(request, 'bespoke/bespoke_success.html')
