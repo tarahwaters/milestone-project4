4ab72bfb15d143a6975e701c62dcb1a27e872a5f
Tarah Waters <thbwaters@outlook.com>
2024-07-19 13:42:37 +0000

    Amend project views and urls  test custom 404 and 500 error pages

--- nyreewaters_art/urls.py
+++ nyreewaters_art/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from django.shortcuts import render
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -31,9 +37,9 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('bespoke/', include('bespoke.urls')),
     path('about/', include('about.urls')),
-    path('trigger-404/', views.trigger_404, name='trigger_404'),
-    path('trigger-500/', views.trigger_500, name='trigger_500'),
+    path('trigger-404/', lambda request: render(request, '404.html', status=404)),
+    path('trigger-500/', lambda request: render(request, '500.html', status=500)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'nyreewaters_art.views.custom_404'
-handler500 = 'nyreewaters_art.views.custom_500'
+handler404 = 'nyreewaters_art.urls.custom_404'
+handler500 = 'nyreewaters_art.urls.custom_500'
--- nyreewaters_art/views.py
+++ nyreewaters_art/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponseServerError
 
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
-
-def custom_500(request):
-    return render(request, '500.html', status=500)
-
-def trigger_404(request):
-    raise Http404("This is a test 404 error")
-
-def trigger_500(request):
-    1 / 0
+def trigger_500_view(request):
+    """ View used to test the Custom 500 Server Error page """
+    return HttpResponseServerError(render(request, '500.html'))
