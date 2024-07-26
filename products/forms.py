93faad7dbeab0b0e852dd09f95de587a0b4c8f86
Tarah Waters <thbwaters@outlook.com>
2024-07-26 14:56:51 +0000

    Add comment and fix line 12 length

--- products/forms.py
+++ products/forms.py
@@ -9,13 +9,15 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image',
+                             required=False, 
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
+        # update form category field to the friendly name
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
