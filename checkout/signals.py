7e0fe2ea281219972d345c7a5e000c8b69dacbda
Tarah Waters <thbwaters@outlook.com>
2024-07-27 13:15:58 +0000

    Validate checkout signals.py to PEP8

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+
 # updates when the post_save signal is sent
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
@@ -10,6 +11,7 @@ def update_on_save(sender, instance, created, **kwargs):
     Update order total on lineitem update/create
     """
     instance.order.update_total()
+
 
 # updates when the post_delete signal is sent
 @receiver(post_delete, sender=OrderLineItem)
