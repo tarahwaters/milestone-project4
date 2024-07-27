6122edfa7a90f47cd343eb1232914f00cd57e28c
Tarah Waters <thbwaters@outlook.com>
2024-07-27 13:13:52 +0000

    Correct error in signal for post_delete and add comments

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+# updates when the post_save signal is sent
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
@@ -10,7 +11,8 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     instance.order.update_total()
 
-@receiver(post_save, sender=OrderLineItem)
+# updates when the post_delete signal is sent
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
