45a48356ebeb84b1de94b376b67e1959f549db60
Tarah Waters <thbwaters@outlook.com>
2024-07-27 21:11:50 +0000

    Add missing view description

--- faqs/views.py
+++ faqs/views.py
@@ -3,5 +3,6 @@ from .models import FAQ
 
 
 def faq_list(request):
+    """ A view to return the FAQs page """
     faqs = FAQ.objects.all()
     return render(request, 'faqs/faq_list.html', {'faqs': faqs})
