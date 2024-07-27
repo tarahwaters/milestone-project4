e33f6374309c4435a0c31061b1749c4b3a662c7a
Tarah Waters <thbwaters@outlook.com>
2024-07-27 15:32:52 +0000

    Make PEP3 compliant changes to profiles files

--- profiles/models.py
+++ profiles/models.py
@@ -12,15 +12,24 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_first_name = models.CharField(max_length=40, null=True, blank=True)
-    default_last_name = models.CharField(max_length=40, null=True, blank=True)
-    default_mobile_number = models.CharField(max_length=20, null=True, blank=True)
-    default_address_line1 = models.CharField(max_length=80, null=True, blank=True)
-    default_address_line2 = models.CharField(max_length=80, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_country = CountryField(blank_label="Country", null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_first_name = models.CharField(
+            max_length=40, null=True, blank=True)
+    default_last_name = models.CharField(
+            max_length=40, null=True, blank=True)
+    default_mobile_number = models.CharField(
+            max_length=20, null=True, blank=True)
+    default_address_line1 = models.CharField(
+            max_length=80, null=True, blank=True)
+    default_address_line2 = models.CharField(
+            max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+            max_length=40, null=True, blank=True)
+    default_county = models.CharField(
+            max_length=80, null=True, blank=True)
+    default_country = CountryField(
+            blank_label="Country", null=True, blank=True)
+    default_postcode = models.CharField(
+            max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
