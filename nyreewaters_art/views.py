ea91a071a8d74c4ba0afba5df1c7cf0d04aef6da
Tarah Waters <thbwaters@outlook.com>
2024-07-27 15:14:44 +0000

    Make PEP3 compliant changes

--- nyreewaters_art/views.py
+++ nyreewaters_art/views.py
@@ -5,9 +5,11 @@ def custom_403(request, exception):
     """ Error Handler 403 """
     return render(request, '403.html', status=404)
 
+
 def custom_404(request, exception):
     """ Error Handler 404 """
     return render(request, '404.html', status=404)
+
 
 def custom_500(request):
     """ Error Handler 500 """
