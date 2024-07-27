5564ca8657fcecf4fe95e20da33046a1574636fe
Tarah Waters <thbwaters@outlook.com>
2024-07-27 15:27:36 +0000

    Make PEP3 compliant changes to faqs files

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import FAQ
 
+
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
     list_display = ('question', 'category', 'created_at')
--- faqs/views.py
+++ faqs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import FAQ
 
+
 def faq_list(request):
     faqs = FAQ.objects.all()
     return render(request, 'faqs/faq_list.html', {'faqs': faqs})
